e3cfc45091af5d0180fbd33e3b8b6f05fdfd122e
evalece <60049803+evalece@users.noreply.github.com>
2025-04-12 23:28:54 -0400

    rename frontend to app

--- frontend/graphQL/src/config.js
+++
@@ -1,6 +0,0 @@
-// subject to script control
-export default  {
-    Redis_URL: "redis://localhost:6380",
-    GraphQLPort: 4000
-
-  }
--- frontend/restAPI/fastAPI/app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from api.routes import router as session_router
-app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"msg": "FastAPI on!"}
-app.include_router(session_router)
